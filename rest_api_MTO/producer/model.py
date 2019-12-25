86f86e03386ab6b6bbc05e9b4fa889abf5e1b659
gaurshetty <gaurshetty@gmail.com>
2019-12-25 18:30:38 +0530

    updating models in ManyToOne framework

--- rest_api_MTO/producer/model.py
+++ rest_api_MTO/producer/model.py
@@ -1,34 +1,33 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from rest_api_MTO.producer.dbconfig import *
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/pco'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-# one_to_one
+# ManyToOne
 class Customer(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column("name", db.String(100))
+    name = db.Column("name", db.String(100), nullable=False)
     age = db.Column("age", db.Integer)
-    gender = db.Column("gender", db.String(50))
+    gender = db.Column("gender", db.String(20))
     email = db.Column("email", db.String(100), unique=True)
     active = db.Column("active", db.String(10), default='Y')
-    addresses = db.relationship("Address", uselist=False, backref="customers", lazy=False)
+    aid = db.Column("aid", db.ForeignKey("Address.id"), unique=False, nullable=False)
 
     def __str__(self):
-        return f'{self.__dict__}'
+        return str(self)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+
 class Address(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
-    city = db.Column("city", db.String(100))
-    state = db.Column("state", db.String(100))
+    city = db.Column("city", db.String(100), nullable=False)
+    State = db.Column("State", db.String(100))
     pincode = db.Column("pincode", db.Integer, unique=True)
     active = db.Column("active", db.String(10), default='Y')
-    cid = db.Column("cid", db.ForeignKey("customer.id"), unique=True, nullable=False)
+    customers = db.relationship("Customer", uselist=True, backref="addresses", lazy=True)
+
+    def __str__(self):
+        return str(self)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
